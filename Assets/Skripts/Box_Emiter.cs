fa26d2c9480f317753576d1622d0c2a139e6a3b1
Walfenstaine <Pavlik-gureev@mail.ru>
2023-05-24 12:26:19 +0300

    1112123

---
+++ Assets/Skripts/Box_Emiter.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Box_Emiter : MonoBehaviour
+{
+    public AudioClip clip;
+    public Animator anim;
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            SoundPlayer.regit.sorse.PlayOneShot(clip);
+            anim.SetBool("Open", true);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            SoundPlayer.regit.sorse.PlayOneShot(clip);
+            anim.SetBool("Open", false);
+        }
+    }
+}
+
